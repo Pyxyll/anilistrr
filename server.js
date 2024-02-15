7712fabc67492edded3e7164b56da8a34684cf3b
Glitch (glitch-hello-node) <none>
2024-02-15 19:23:45 +0000

    🌽👽 Checkpoint ./package.json:59551510/536 ./server.js:59551510/901

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import routes from "./routes.js";
+import { create } from "express-handlebars";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+const handlebars = create({extname: '.hbs'});
+app.engine(".hbs", handlebars.engine);
+app.set("view engine", ".hbs");
+
+app.use("/", routes);
+
+app.listen(port, () => console.log(`anilist is listening on port ${port}`)
