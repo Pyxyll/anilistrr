d351aeb5f16634e0cf06dd2d7c26e34cd75b4836
Glitch (glitch-hello-node) <none>
2024-02-23 21:30:59 +0000

    🍇😋 Checkpoint ./models/anime-store.json:59551510/4 ./controllers/show.js:59551510/275

--- controllers/show.js
+++ controllers/show.js
@@ -7,11 +7,14 @@ const show = {
   createView(request, response) {
     const id = request.params.id;
     logger.debug('show id = ' + id);
+    logger.debug(animeStore.getShow(id));
     
     const viewData = {
       title: 'show',
-      singleShow: animeStore.getShow(id)
+      singleShow: animeStore.getShow(id),
+
     };
+          logger.debug(animeStore.getShow(id))
 
     response.render('show', viewData);
   },
